0706e3e210d076ccf09cb94e9d21ee7e98472eff
vignesh457 <91398734+vignesh457@users.noreply.github.com>
2022-05-28 17:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-document.querySelector(".mode").addEventListener("click",function(){
-  var curr = document.querySelector(".style-link").getAttribute("href");
-  if(curr=="css/style.css")
-  {
-    document.querySelector(".mode").setAttribute("src","https://cdn-icons-png.flaticon.com/128/1177/1177480.png");
-    document.querySelector(".style-link").setAttribute("href","css/style2.css");
-  }
-  else
-  {
-    document.querySelector(".mode").setAttribute("src","https://t4.ftcdn.net/jpg/02/40/37/31/240_F_240373136_O2ZOgz62EtgA1w6KVlsktHpIzeYFqv24.jpg");
-    document.querySelector(".style-link").setAttribute("href","css/style.css");
-  }
-
-});
+document.querySelector(".mode").addEventListener("click",function(){var curr=document.querySelector(".style-link").getAttribute("href");if(curr=="css/style.css"){document.querySelector(".mode").setAttribute("src","https://cdn-icons-png.flaticon.com/512/2261/2261170.png");document.querySelector(".style-link").setAttribute("href","css/style2.css")}else{document.querySelector(".mode").setAttribute("src","https://cdn-icons-png.flaticon.com/128/2925/2925464.png");document.querySelector(".style-link").setAttribute("href","css/style.css")}})
